6cca0ee66b8533c635133727832ae231151a0393
James Olsen <olsen.159@osu.edu>
2018-02-07 20:27:29 -0500

    Added models and controllers for api

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fisher.Bookstore.Api.Models
+{
+    public class Author
+    {
+        public int Id{get; set;}
+        public string Name{get; set;}
+        public string Nickname{get; set;}
+    }
+}
